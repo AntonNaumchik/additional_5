dc67778613a1053dce50d82cd678f80cfeb50f86
Anton <huliovalente@gmail.com>
2018-09-30 15:57:44 +0300

    ver.1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+    if (str.length % 2 !== 0)
+        return false;
+    if (str.length === 0) {
+        return true;
+
+    } else {
+        return false;
+    }
 }
